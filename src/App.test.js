3008798ff6031ce67f7b25ab371c6bf616f66dda
Mosharraf Musa <mosharrafny@gmail.com>
2020-04-16 19:37:40 -0400

    First Test Passed

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test('first name* is on form', async () => {
+  const {getByText} = render(<App />);
+  const firstName = getByText(/first name*/i);
+  expect(firstName).toBeInTheDocument();
 });
