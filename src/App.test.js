d5eb65c684436f8cb981a2f8bab02fc664e23c4b
Mosharraf Musa <mosharrafny@gmail.com>
2020-04-16 21:00:48 -0400

    Changing input value testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,42 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, getByLabelText, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
 //test getByText
-test('first name* is on form', async () => {
-  const {getByText} = render(<App />);
+test("first name* is on form", async () => {
+  const { getByText } = render(<App />);
   const firstName = getByText(/first name*/i);
   expect(firstName).toBeInTheDocument();
 });
 
-test('last name is on the form', () => {
-  const {getByText} = render(<App />);
+test("last name is on the form", () => {
+  const { getByText } = render(<App />);
   const lastName = getByText(/last name*/i);
-  expect(lastName).toBeInTheDocument()
+  expect(lastName).toBeInTheDocument();
 });
 
-test('email is on the form', () => {
-  const {getByText} = render(<App />);
+test("email is on the form", () => {
+  const { getByText } = render(<App />);
   const email = getByText(/email*/i);
   expect(email).toBeInTheDocument();
-}); 
+});
+
+test("form submit adds new info to the list", () => {});
+
+//Testing input value change on contact form
+
+test("Changing input values", () => {
+  const { getByLabelText, getByTestId } = render(<ContactForm />);
+
+  const firstNameInput = getByLabelText(/First Name*/);
+
+  fireEvent.change(firstNameInput, { target: { value: "Nel" } });
+
+  expect(firstNameInput.value).toBe("Nel");
+
+  fireEvent.click(getByTestId(/test/i));
+});
