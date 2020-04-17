bbaef7d942128652c2b368fa3b10eed9584b8a21
Mosharraf Musa <mosharrafny@gmail.com>
2020-04-16 20:04:01 -0400

    test passed by getByText

--- src/App.test.js
+++ src/App.test.js
@@ -5,9 +5,21 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
-
+//test getByText
 test('first name* is on form', async () => {
   const {getByText} = render(<App />);
   const firstName = getByText(/first name*/i);
   expect(firstName).toBeInTheDocument();
 });
+
+test('last name is on the form', () => {
+  const {getByText} = render(<App />);
+  const lastName = getByText(/last name*/i);
+  expect(lastName).toBeInTheDocument()
+});
+
+test('email is on the form', () => {
+  const {getByText} = render(<App />);
+  const email = getByText(/email*/i);
+  expect(email).toBeInTheDocument();
+}); 
